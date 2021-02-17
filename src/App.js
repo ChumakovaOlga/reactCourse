ab20e7ff36977bfe6eb2548a6250efe8bd82e581
Chumakova Olga <tresa1@mail.ru>
2021-02-17 19:34:12 +0200

    React 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Greeting from './Greeting/greeting.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        greetings: [
+            {text: 'Привет!'},
+            {text: 'Как дела?'},
+        ]
+    }
+    addText = () => {
+        let greetings = this.state.greetings;
+        greetings.push({text: 'Нормально'});
+        this.setState({
+            greetings: greetings
+        })
+    }
+
+    render() {
+        const greetings =  this.state.greetings;
+        let greetingsText = greetings.map((el) =>
+            <Greeting text={el.text}/>
+        );
+     return (
+        <div>
+            {greetingsText}
+            <button onClick={this.addText}>Добавить текст</button>
+       </div>
+     );
+    }
 }
 
 export default App;
