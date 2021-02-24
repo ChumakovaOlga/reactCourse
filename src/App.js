48d50d3fe08aebe9cdc4e3dbe37aecbce197e391
Chumakova Olga <tresa1@mail.ru>
2021-02-24 21:27:16 +0200

    React 3

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 import Greeting from './Greeting/Greeting.js'
+import './Greeting/Greeting.css';
+import Button from '@material-ui/core/Button';
+import Header from './Header/Header'
+// import TextField from '@material-ui/core/TextField';
+
+
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        // создадим ref в поле `textInput` для хранения DOM-элемента
+        this.textInput = React.createRef();
+    }
+
+
     state = {
         greetings: [
-            {text: 'Привет!', name: 'John'},
-            {text: 'Как дела?', name: 'Pete'},
+            {text: 'Привет!', name: 'robot'},
+            {text: 'Как дела?', name: 'robot'},
         ],
-        textMessage: '',
-        pageTitle: ''
+        input: ''
     }
-    addText = () => {
-        const newGreetings = [...this.state.greetings, {text: 'Нормально!', name: 'Mike'}]
-        this.setState({
-            greetings: newGreetings
-        })
-    }
-    showNameHandler = (newTitle) => {
-        this.setState({
-            pageTitle: newTitle
-        })
-    }
-    handleInput = (event) => {
-        this.setState({
-            textMessage: event.target.value
-        })
+    // Ставим фокус на <input> при монтировании компонента
+    componentDidMount() {
+        this.textInput.current.focus();
     }
     componentDidUpdate() {
-        if (this.state.greetings.length % 2 === 1) {
+            if (this.state.greetings[this.state.greetings.length - 1].name === 'me')
+        {
             setTimeout(() =>
                 this.setState(
                     {
@@ -36,30 +37,84 @@ class App extends Component {
                     }), 1000)
         }
     }
-    render() {
-        const divStyle = {
-            textAlign: 'center'
+    handleClick = (greeting) => {
+        this.sendMessage(greeting)
+        // this.setState({ greetings: [ ...this.state.greetings, {text: greeting, name: 'me'} ] });
+    };
+    handleChange = (event) => {
+        this.setState({input: event.target.value})
+    }
+    handleKeyUp = (event, greeting) => {
+        if (event.keyCode === 13) {
+            this.sendMessage(greeting)
+            this.setState({greetings: [...this.state.greetings, {text: greeting, name: 'me'}]})
         }
-        const greetings =  this.state.greetings;
-        let greetingsText = greetings.map((greeting, index) =>
-            <Greeting
-                key = {index}
-                text = {greeting.text}
-                name = {greeting.name}
-                onChangeName={() => this.showNameHandler( greetings[index].name)}
-            />
-        );
+    }
+    sendMessage = (greeting) => {
+    this.setState({greetings: [...this.state.greetings, {text: greeting,name: 'me'}],
+        input: ''
+    })
+    }
+        render() {
+        const greetingsText = this.state.greetings.map((greeting, index) =>
+            (
+
+                     <Greeting
+                     key = {index}
+                     text = {greeting.text}
+                     name = {greeting.name}
+                     />
+
+            ))
+
      return (
-         <div style={divStyle}>
+             <div  id='main' className="layout">
+                 <Header/>
+                  <div className='message-field'>
+                     {greetingsText}
+                   </div>
+                 <div style={{width: '100%', display: 'flex'}}>
 
-             <h2>Сообщение отправил: {this.state.pageTitle}</h2>
-             <h3>Текст сообщения:{this.state.textMessage}</h3>
-             <input type="text" onChange={this.handleInput}/>
+                     {/*<TextField*/}
+                     {/*    ref={this.textInput}*/}
+                     {/*    name = 'input'*/}
+                     {/*    id="outlined-basic"*/}
+                     {/*    label="Outlined" variant="outlined"*/}
+                     {/*    className='text' type="text"*/}
+                     {/*    onChange={this.handleChange}*/}
+                     {/*    value={this.state.input}*/}
+                     {/*    onKeyUp={(event) =>*/}
+                     {/*          this.handleKeyUp(event, this.state.input)}*/}
+                     {/*/>*/}
 
-             {greetingsText}
 
-             <button onClick={this.addText}>Добавить текст</button>
-       </div>
+                         <input
+                             ref={this.textInput}
+                             name = 'input'
+                                className='text' type="text"
+                                onChange={this.handleChange}
+                                value={this.state.input}
+                                onKeyUp={(event) =>
+                                    this.handleKeyUp(event, this.state.input)}
+                         />
+
+
+
+                         {/*<button className='button'*/}
+                         {/*        onClick={ () => this.handleClick(this.state.input)}*/}
+                         {/*>*/}
+                         {/*    Отправить*/}
+                         {/*</button>*/}
+
+                     <Button variant="contained"
+                             className='button'
+                             onClick={ () => this.handleClick(this.state.input)}
+                     >
+                         Send
+                     </Button>
+
+                 </div>
+             </div>
      );
     }
 }
--- src/Greeting/Greeting.js
+++ src/Greeting/Greeting.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import Avatar from '@material-ui/core/Avatar';
+
 
 export default props => (
+    <div className='message'
+         style={ { alignSelf: props.name === 'robot' ?
+                 'flex-start' : 'flex-end',
 
+         } }
+    >
+        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg"  />
+        <div>{props.text}</div>
+        <div className='message-sender'>{props.name}</div>
 
-    <div style={{
-        border: '1px solid #ccc',
-        marginBottom: '10px'
-    }}>
-    <p>Текст: {props.text}</p>
-    <p>{props.name}</p>
-    <button onClick={props.onChangeName}>Отправить</button>
     </div>
     )
-
